2deab18b72ebdf95b5acae2e009c012e9f904bcc
Jordan Suchow <suchow@post.harvard.edu>
2017-09-27 22:41:44 -0400

    Add method to register base URL

--- judicious/__init__.py
+++ judicious/__init__.py
@@ -5,3 +5,13 @@
 __author__ = """Jordan W. Suchow"""
 __email__ = 'jwsuchow@gmail.com'
 __version__ = '0.1.0'
+
+from judicious import (
+    BASE_URL,
+    register,
+)
+
+__all__ = (
+    "BASE_URL",
+    "register",
+)
--- judicious/judicious.py
+++ judicious/judicious.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import os
+import uuid
+
+import requests
+
+BASE_URL = os.environ.get("JUDICIOUS_URL", "http://imprudent.herokuapp.com")
+
+UUID = str(uuid.uuid4())
+
+
+def register(url):
+    """Set the base URL of the decision server."""
+    os.environ["JUDICIOUS_URL"] = url
+
