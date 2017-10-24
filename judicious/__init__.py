7c986c2536a7968cdce912fc917f77a5754c1149
Jordan Suchow <suchow@post.harvard.edu>
2017-10-24 02:01:42 -0400

    Register server automatically
    Use environment variable JUDICIOUS_SERVER_URL, otherwise default to local host.

--- judicious/__init__.py
+++ judicious/__init__.py
@@ -6,6 +6,7 @@ __author__ = """Jordan W. Suchow"""
 __email__ = 'jwsuchow@gmail.com'
 __version__ = '0.1.0'
 
+import os
 
 from .core import (
     base_url,
@@ -44,6 +45,8 @@ from .tasks import (
 from . import tasks
 
 from .person import Person
+
+register(os.environ.get("JUDICIOUS_SERVER_URL", "http://127.0.0.1:5000"))
 
 
 __all__ = (
---
+++ tests/test29.py
@@ -0,0 +1,3 @@
+import judicious
+
+j = judicious.joke()
