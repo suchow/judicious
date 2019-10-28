c55e41b8d6e6bd781a7c0df38d604d35a8187542
stefanuddenberg <stefan.uddenberg@gmail.com>
2019-10-28 14:52:44 -0400

    Added version most recently piloted

--- experiments/trustworthiness_and_attractiveness_pilot.py
+++ experiments/trustworthiness_and_attractiveness_pilot.py
@@ -13,7 +13,8 @@ import judicious
 
 # judicious.seed("c65a01e7-079e-4e91-8a1b-cac8c5f719d9")
 # judicious.seed("46b14020-5ec6-4e05-8bb7-ceb1202f3559")
-judicious.seed("fd8a2ed1-bb91-4d59-b805-c94dace586ee")
+# judicious.seed("fd8a2ed1-bb91-4d59-b805-c94dace586ee")
+judicious.seed("d8b75930-db30-47d9-b208-72d6ffe5274b")
 
 
 def trial_sequences(n_stimuli, n_ratings, n_trials, n_retests, shuffle_sequences=False):
